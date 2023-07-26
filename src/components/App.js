9b68a4129f18141892ac5ef491a6e03c789e9523
josphat <mjirura0707@gmail.com>
2023-07-26 16:12:37 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import "./App.css";
 
 function App() {
-
+  function darkMode() {
+    setDarkMode((prevMode)=> !prevMode);
+    
+  }
+  
+  const[isDarkMode, setDarkMode]= useState(false);
+  const appClass = isDarkMode ? "app dark":"app light";
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  //const appClass = false ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  function handleCartToggle() {
+    setInCart((prevInCart) => !prevInCart);
+  }
+
+  const [inCart, setInCart] = useState(false);
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
